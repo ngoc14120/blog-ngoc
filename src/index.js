72fb29dd7b723cac68f1a15203c8149f17d7b69a
Phan Ngoc <tienngoc200050@gmail.com>
2022-07-13 23:04:14 +0700

    Form default behavior

--- src/index.js
+++ src/index.js
@@ -22,6 +22,10 @@ app.get("/", (req, res) => {
 app.get("/news", (req, res) => {
   res.render("news");
 });
+app.get("/search", (req, res) => {
+  res.render("search");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
